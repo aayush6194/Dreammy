9f9bf98616d78a5a9e8779103fe681f07b111d01
aayush6194 <aayush6194@gmail.com>
2019-04-23 19:55:06 -0500

    social media links in new tab

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -8,13 +8,13 @@ const twitterUrl = "https://www.twitter.com/";
 const SocialLinks = ({facebook, instagram, twitter})=>(
   <div>
   {facebook?
-     <a href={facebookUrl + facebook}><i className="fab fa-facebook txt-xl"></i></a>
+     <a  target="_blank" href={facebookUrl + facebook}><i className="fab fa-facebook txt-xl"></i></a>
      :<i className="fab fa-facebook txt-xl disabled"></i>}&nbsp;&nbsp;
   {instagram?
-   <a href={instagramUrl + instagram}><i className="fab fa-twitter-square txt-xl"></i></a>
+   <a  target="_blank" href={instagramUrl + instagram}><i className="fab fa-twitter-square txt-xl"></i></a>
    :<i className="fab fa-twitter-square txt-xl"></i>}&nbsp;&nbsp;
    {twitter?
-    <a href={twitterUrl + twitter}><i className="fab fa-twitter-square txt-xl"></i></a>
+    <a  target="_blank" href={twitterUrl + twitter}><i className="fab fa-twitter-square txt-xl"></i></a>
     :<i className="fab fa-linkedin txt-xl disabled"></i>}&nbsp;&nbsp;
     </div>
 )
